d424b880eb9255a2e951d760eb2d81aed0776e09
Kaushik Devireddy <kaushik@dassana.io>
2021-10-04 14:55:59 -0700

    Update handler.py

--- content/actions/get-iam-policy-risks/src/handler.py
+++ content/actions/get-iam-policy-risks/src/handler.py
@@ -1,5 +1,6 @@
 from json import load, loads, dumps
 from typing import Dict, Any
+import time
 
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from aws_lambda_powertools.utilities.validation import validator
@@ -12,6 +13,10 @@ from cloudsplaining.output.policy_finding import PolicyFinding
 with open('input.json', 'r') as schema:
     schema = load(schema)
     dassana_aws = DassanaAwsObject()
+    
+def cloudsplain_policy(policy):
+    pass
+
 
 @validator(inbound_schema=schema)
 def handle(event: Dict[str, Any], context: LambdaContext):
@@ -19,6 +24,8 @@ def handle(event: Dict[str, Any], context: LambdaContext):
     policies = []
     all_findings = {}
     exclusions_config = {}
+    
+    latency = 0
     
     iam_arn = event.get('iamArn')
     client = dassana_aws.create_aws_client(context, 'iam', event.get('region'))
@@ -59,6 +66,7 @@ def handle(event: Dict[str, Any], context: LambdaContext):
                     })
         except Exception:
             pass
+        
     elif prefix[-1] == 'user':
         paginator = client.get_paginator('list_attached_user_policies')
         
@@ -90,6 +98,7 @@ def handle(event: Dict[str, Any], context: LambdaContext):
                     })
         except Exception:
             pass
+        
     elif prefix[-1] == 'policy':
         policies.append({
             'PolicyArn':iam_arn, 
@@ -101,7 +110,6 @@ def handle(event: Dict[str, Any], context: LambdaContext):
             policy_basic = client.get_policy(
                 PolicyArn=policy['PolicyArn']
             )
-            
             policy_detailed = client.get_policy_version(
                 PolicyArn=policy['PolicyArn'],
                 VersionId=policy_basic['Policy']['DefaultVersionId']
@@ -121,6 +129,7 @@ def handle(event: Dict[str, Any], context: LambdaContext):
         policy_finding = PolicyFinding(policy_document, exclusions)
         
         policy['PolicyFindings'] = policy_finding.results
+
         
         for finding_category in policy_finding.results.keys():
             if finding_category in all_findings:
@@ -135,12 +144,12 @@ def handle(event: Dict[str, Any], context: LambdaContext):
                     all_findings[finding_category] = set(policy_finding.results[finding_category])
                 else:
                     all_findings[finding_category] = set([perm for type in policy_finding.results[finding_category] for perm in type['actions']])
-    
+        
     all_findings = {category: list(findings) for category, findings in all_findings.items()}
-    
+    print("Total Latency")
+    print(latency)
     response = dumps({
-        'PolicyFindings': all_findings,
-        'Policies': policies
+        'PolicyFindings': all_findings
     }, default=str)
     
     return {"result": loads(response)}
