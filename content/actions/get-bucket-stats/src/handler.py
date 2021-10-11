0b6ae39902daf5d1d6a7470089d02e84c6ff4aec
Kaushik Devireddy <kaushik@dassana.io>
2021-10-10 17:01:22 -0700

    refactoring + comments

--- content/actions/get-bucket-stats/src/handler.py
+++ content/actions/get-bucket-stats/src/handler.py
@@ -10,56 +10,48 @@ from dassana.common.cache import configure_ttl_cache
 with open('input.json', 'r') as schema:
     schema = load(schema)
     dassana_aws = DassanaAwsObject()
-
+    
 get_cached_client = configure_ttl_cache(1024, 60)
+    
+def get_storage_metric(client, bucket_name, metric_name, storage_type):
+    now = datetime.datetime.utcnow()
+    
+    response = client.get_metric_statistics(Namespace='AWS/S3',
+                                        MetricName=metric_name,
+                                        Dimensions=[
+                                            {'Name': 'BucketName', 'Value': bucket_name},
+                                            {'Name': 'StorageType', 'Value': storage_type}
+                                        ],
+                                        Statistics=['Average'],
+                                        Period=86400,
+                                        StartTime=(now-datetime.timedelta(days=1)).isoformat(),
+                                        EndTime=(now-datetime.timedelta(days=0)).isoformat()
+                                        )
+    
+    return loads(dumps(response, default=str))
 
 @validator(inbound_schema=schema)
 def handle(event: Dict[str, Any], context: LambdaContext):
     cw_client = get_cached_client(dassana_aws.create_aws_client, context=context, service='cloudwatch',
                                region=event.get('region'))
-
-    now = datetime.datetime.utcnow()
-
-    response = cw_client.get_metric_statistics(Namespace='AWS/S3',
-                                        MetricName='BucketSizeBytes',
-                                        Dimensions=[
-                                            {'Name': 'BucketName', 'Value': event.get('bucketName')},
-                                            {'Name': 'StorageType', 'Value': 'StandardStorage'}
-                                        ],
-                                        Statistics=['Average'],
-                                        Period=86400,
-                                        StartTime=(now-datetime.timedelta(days=1)).isoformat(),
-                                        EndTime=(now-datetime.timedelta(days=0)).isoformat()
-                                        )
     
-    cw_response = loads(dumps(response, default=str))
-    
+    cw_metric = get_storage_metric(cw_client, event.get('bucketName'),'BucketSizeBytes', 'StandardStorage')
+
     try:
-        bucketSize = response['Datapoints'][0]['Average']
+        bucketSize = cw_metric['Datapoints'][0]['Average']
     except Exception:
         bucketSize = 0
     
+    # Convert bucket size from bytes to GB
     for i in range(3):
         bucketSize /= 1024
     
     bucketSize = round(bucketSize, 6)
-        
-    response = cw_client.get_metric_statistics(Namespace='AWS/S3',
-                                        MetricName='NumberOfObjects',
-                                        Dimensions=[
-                                            {'Name': 'BucketName', 'Value': event.get('bucketName')},
-                                            {'Name': 'StorageType', 'Value': 'AllStorageTypes'}
-                                        ],
-                                        Statistics=['Average'],
-                                        Period=86400,
-                                        StartTime=(now-datetime.timedelta(days=1)).isoformat(),
-                                        EndTime=(now-datetime.timedelta(days=0)).isoformat()
-                                        )
-                                        
-    cw_response = loads(dumps(response, default=str))
+    
+    cw_metric = get_storage_metric(cw_client, event.get('bucketName'), 'NumberOfObjects', 'AllStorageTypes')
     
     try:
-        numberOfObjects = response['Datapoints'][0]['Average']
+        numberOfObjects = cw_metric['Datapoints'][0]['Average']
     except Exception:
         numberOfObjects = 0
     
